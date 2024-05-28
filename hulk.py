f397b5eca2a13154746c318028f296835483127d
affanatmaca <trpxzf@gmail.com>
2024-05-28 19:37:37 +0300

    Update hulk.py

--- hulk.py
+++ hulk.py
@@ -1,13 +1,3 @@
-# ----------------------------------------------------------------------------------------------
-# HULK - HTTP Unbearable Load King
-#
-# this tool is a dos tool that is meant to put heavy load on HTTP servers in order to bring them
-# to their knees by exhausting the resource pool, its is meant for research purposes only
-# and any malicious usage of this tool is prohibited.
-#
-# author :  Barry Shteiman , version 1.0
-# Updated for Python 3 compatibility
-# ----------------------------------------------------------------------------------------------
 import urllib.request
 import sys
 import threading
